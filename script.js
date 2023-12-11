9861652c63a38aa1a4b30c3da7aec049394e7d16
Adarsh Tiwari <144592420+aadi0608@users.noreply.github.com>
2023-12-11 18:36:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function performSearch() {
+    const searchTerm = document.getElementById('searchInput').value;
+    const searchResults = document.getElementById('searchResults');
+  
+    // Simulate search results - replace this with actual functionality
+    searchResults.innerHTML = `<p>Showing search results for: ${searchTerm}</p>`;
+  }
+  
